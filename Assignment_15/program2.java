a4e30b8cafb3974198e186093b83c68d9acb7d95
SurajGardi <surajgardi0707@gmail.com>
2025-05-27 17:13:21 +0530

    Assignment 15 added

---
+++ Assignment_15/program2.java
@@ -0,0 +1,54 @@
+// Input 4 4
+
+/*
+    *       *       *       *
+    *       *       *       #
+    *       *       #       #
+    *       #       #       #
+ */
+
+ import java.util.*;
+
+ class Pattern
+ {
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0, iCnt = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = iCol; j >= 1; j--)
+            {
+                if((i == j) || (i < j))
+                {
+                    System.out.print("*"+"\t");
+                }
+                else
+                {
+                    System.out.print("#"+"\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+ }
+
+class program1
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter number of Rows and Columns : ");
+
+        Scanner sobj = new Scanner(System.in);
+
+        iValue1 = sobj.nextInt();
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+    }
+}
