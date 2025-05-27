d1ac4e8709c36387e2ce81b42c4668c7b058ade3
SurajGardi <surajgardi0707@gmail.com>
2025-05-27 17:14:11 +0530

    Assignment 16 added

---
+++ Assignment_16/program3.c
@@ -0,0 +1,50 @@
+// input : 5 5
+
+/*
+    $       *       *       *       *
+    #       $       *       *       *
+    #       #       $       *       *
+    #       #       #       $       *
+    #       #       #       #       $
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iCol; i++)
+    {
+        for(j = 1; j <= iRow; j++)
+        {
+            if(i == j)
+            {
+                printf("$ \t");
+            }
+            else if(j < i)
+            {
+                printf("# \t");
+            }
+            else
+            {
+                printf("* \t");
+            }
+
+        }
+        printf("\n");
+    }
+
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter Rows and Columns : \n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
---
+++ Assignment_16/program5.c
@@ -0,0 +1,44 @@
+// input : 4 4
+
+/*
+    *       *       *       *
+            *       *       *
+                    *       *
+                            *
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iCol; i++)
+    {
+        for(j = 1; j <= iRow; j++)
+        {
+            if((i == j) || (i < j))
+            {
+                printf("* \t");
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n");
+    }
+
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter Rows and Columns : \n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
